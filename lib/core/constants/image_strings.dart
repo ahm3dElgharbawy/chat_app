361fa5ff862dcc66313201a339261ab6cc19c44b
ahm3dElgharbawy <ahm3delsafy@gmail.com>
2024-10-01 15:38:31 +0200

    finish splash screen

--- lib/core/constants/image_strings.dart
+++ lib/core/constants/image_strings.dart
@@ -1,4 +1,5 @@
 class ImageStrings {
   ImageStrings._internal();
   static const user = "assets/images/user.png";
+  static const empty = "assets/images/empty.png";
 }
--- lib/features/intro/presentation/views/splash_screen.dart
+++ lib/features/intro/presentation/views/splash_screen.dart
@@ -1,10 +1,60 @@
-import 'package:flutter/widgets.dart';
+import 'package:chat_app/core/constants/icon_strings.dart';
+import 'package:chat_app/core/constants/image_strings.dart';
+import 'package:chat_app/core/constants/routes.dart';
+import 'package:chat_app/core/extensions/navigation.dart';
+import 'package:chat_app/core/extensions/size_extensions.dart';
+import 'package:chat_app/core/themes/styles.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    waitAndGo();
+  }
+
+  // splash screen with duration 2 seconds
+  void waitAndGo() {
+    Future.delayed(const Duration(seconds: 3), () {
+      context.pushNamed(AppRoutes.login);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          FadeInImage(
+            height: 100.h,
+            placeholder: const AssetImage(
+              ImageStrings.empty,
+            ),
+            image: const AssetImage(
+              IconStrings.whatsapp,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 100),
+              child: Text(
+                "Made with 💙 by Ahm3d",
+                style: TextStyles.regular16,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
