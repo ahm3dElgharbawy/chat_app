8f6075d8765e60f1e577f29546f7b79cfa5ee814
ahm3dElgharbawy <ahm3delsafy@gmail.com>
2024-10-21 04:01:01 +0200

    remove scaffold from layout body screens & make app bar shared between these screens

--- lib/features/calls/presentation/views/calls_screen.dart
+++ lib/features/calls/presentation/views/calls_screen.dart
@@ -1,4 +1,3 @@
-import 'package:chat_app/core/common_widgets/main_appbar.dart';
 import 'package:chat_app/features/calls/presentation/views/widgets/calls_body.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +6,6 @@ class CallsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MainAppBar(),
-      body: CallsBody()
-    );
+    return const CallsBody();
   }
 }
--- lib/features/chats/presentation/views/chats_screen.dart
+++ lib/features/chats/presentation/views/chats_screen.dart
@@ -1,11 +1,3 @@
-import 'package:chat_app/core/common_widgets/contact_card.dart';
-import 'package:chat_app/core/common_widgets/main_appbar.dart';
-import 'package:chat_app/core/constants/image_strings.dart';
-import 'package:chat_app/core/constants/routes.dart';
-import 'package:chat_app/core/enums/media_type.dart';
-import 'package:chat_app/core/enums/seen_status.dart';
-import 'package:chat_app/core/extensions/navigation.dart';
-import 'package:chat_app/core/themes/colors.dart';
 import 'package:chat_app/features/chats/presentation/views/widgets/chats_body.dart';
 import 'package:flutter/material.dart';
 
@@ -14,14 +6,6 @@ class ChatsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const MainAppBar(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: AppColors.primary,
-        child: const Icon(Icons.chat),
-      ),
-      body: const ChatsBody()
-    );
+    return const ChatsBody();
   }
 }
